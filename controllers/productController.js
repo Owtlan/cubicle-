92ba5d89e16b191b88e2d753476b601081790c66
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-06-23 16:20:50 +0300

    Add details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,12 +6,14 @@ const router = Router()
 
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Browse'})
+    res.render('home', { title: 'Browse' })
 })
 
 router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create'})
+    res.render('create', { title: 'Create' })
 })
 
-
+router.get('/details/:productId', (req, res) => {
+    res.render('details', { title: 'Product Details' })
+})
 module.exports = router
