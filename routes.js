e7f225f4bfcc93e00a378ed6012d9574bec36f8e
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-06-23 13:51:23 +0300

    Add page titles

--- routes.js
+++ routes.js
@@ -5,8 +5,8 @@ const aboutController = require('./controllers/aboutController')
 
 const router = Router()
 
-router.use('/', productController)
 router.use('/about', aboutController)
+router.use('/', productController)
 router.get('*', (req, res) => {
     res.render('404')
 })
