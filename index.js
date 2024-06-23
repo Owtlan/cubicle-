448f631bed90c5e29724a5e2d6939f6a50118c3e
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-06-23 12:32:35 +0300

    add view engine and home page

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
-
 const config = require('./config/config')
-
+const exphbs = require('express-handlebars');
 const app = express()
+
+
+const hbs = exphbs.create({
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: false })
+});
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
