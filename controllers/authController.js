48a11c285c93a1564792e8bd4534177a07755679
Tsvetomir Belchev <99121376+Owtlan@users.noreply.github.com>
2024-07-05 13:15:17 +0300

    adding MiddleWare Error Handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const authService = require('../services/authService')
+const validator = require('validator')
+
 
 const isAuthenticated = require('../middlewares/isAuthenticated')
 
@@ -13,6 +15,7 @@ router.get('/login', isGuest, (req, res) => {
 router.post('/login', isGuest, async (req, res) => {
 
     const { username, password } = req.body
+
 
     try {
         let token = await authService.login({ username, password })
@@ -30,7 +33,26 @@ router.get('/register', isGuest, (req, res) => {
 })
 
 
-router.post('/register', isGuest, async (req, res) => {
+const isStrongPasswordMiddleware = (req, res, next) => {
+    let password = req.body.password
+
+    let isStrongPassword = validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+    })
+
+
+    if (!isStrongPassword) {
+        return res.render('register', { message: 'You should have strong password' })
+    }
+
+    next()
+}
+
+router.post('/register', isGuest, isStrongPasswordMiddleware, async (req, res) => {
     const { username, password, repeatPassword } = req.body
 
     if (password !== repeatPassword) {
@@ -43,7 +65,7 @@ router.post('/register', isGuest, async (req, res) => {
 
         res.redirect('/auth/login')
     } catch (error) {
-        res.render('register', { error})
+        res.render('register', { error })
     }
 })
 
